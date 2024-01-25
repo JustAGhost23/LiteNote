83824e6842c96893cf3f2180b15e4d936c041f9c
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-01-25 12:29:50 +0530

    build: add starter dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -17,6 +20,12 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
+        }
+    }
+
+    configurations {
+        all {
+            exclude("commons-logging")
         }
     }
 
@@ -51,19 +60,63 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${System.getProperty("kotlinVersion")}")
+
+    // Compose BOM
+    val composeBOMVersion = "2023.08.00"
+    implementation(platform("androidx.compose:compose-bom:${composeBOMVersion}"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:${composeBOMVersion}"))
+
+    // Lifecycle
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    // Compose
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Splash Screen API
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Accompanist - Status Bar
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
+
+    // Navigation
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-compose:${navVersion}")
+
+    // Coroutines
+    val coroutinesVersion = "1.7.1"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${coroutinesVersion}")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    // Live Data
+    val livedataVersion = "2.7.0"
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${livedataVersion}")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+
+    // Room
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    // Hilt
+    val hiltVersion = "2.48"
+    implementation("com.google.dagger:hilt-android:${hiltVersion}")
+    ksp("com.google.dagger:hilt-compiler:${hiltVersion}")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    val composeVersion = "1.3.3"
+    System.setProperty("composeVersion", composeVersion)
+    val kotlinVersion = "1.9.0"
+    System.setProperty("kotlinVersion", kotlinVersion)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${System.getProperty("kotlinVersion")}")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.1" apply false
+    id("com.android.library") version "7.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id ("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
