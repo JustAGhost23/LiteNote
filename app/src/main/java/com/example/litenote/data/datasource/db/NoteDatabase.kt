6c2b0e6cd20d3552150fea22f61514d05eba1c45
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-01-27 15:39:42 +0530

    feat: setup room database with data model

---
+++ app/src/main/java/com/example/litenote/data/datasource/db/NoteDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.litenote.data.datasource.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.litenote.domain.model.Note
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getInstance(context: Context): NoteDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteDatabase::class.java,
+                        "note_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+
+                }
+                return instance
+
+            }
+
+        }
+    }
+
+}
