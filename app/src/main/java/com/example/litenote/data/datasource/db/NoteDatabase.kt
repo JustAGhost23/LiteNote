b2790fd6deb97afb39a6e496b0f6835a91b1c594
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-01-27 15:55:53 +0530

    refactor: migrate room setup to hilt

--- app/src/main/java/com/example/litenote/data/datasource/db/NoteDatabase.kt
+++ app/src/main/java/com/example/litenote/data/datasource/db/NoteDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.litenote.data.datasource.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.litenote.domain.model.Note
 
@@ -12,26 +10,7 @@ abstract class NoteDatabase : RoomDatabase() {
     abstract fun NoteDAO(): NoteDAO
 
     companion object {
-        @Volatile
-        private var INSTANCE: NoteDatabase? = null
-
-        fun getInstance(context: Context): NoteDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        NoteDatabase::class.java,
-                        "note_database"
-                    ).fallbackToDestructiveMigration().build()
-                    INSTANCE = instance
-
-                }
-                return instance
-
-            }
-
-        }
+        const val DATABASE_NAME = "note_database"
     }
 
 }
--- app/src/main/java/com/example/litenote/presentation/di/core/AppModule.kt
+++ app/src/main/java/com/example/litenote/presentation/di/core/AppModule.kt
@@ -1,11 +1,26 @@
 package com.example.litenote.presentation.di.core
 
+import android.app.Application
+import androidx.room.Room
+import com.example.litenote.data.datasource.db.NoteDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app.applicationContext,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
 
 }
