48b2b8232d3d209bd2c3ebb67f8930d497151045
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-01-29 19:06:31 +0530

    style: add typography

--- app/src/main/java/com/example/litenote/ui/theme/Type.kt
+++ app/src/main/java/com/example/litenote/ui/theme/Type.kt
@@ -2,33 +2,69 @@ package com.example.litenote.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.litenote.R
 
-// Set of Material typography styles to start with
+val AvenirLT = FontFamily(
+    Font(
+        resId = R.font.avenir_lt_book,
+        weight = FontWeight.Normal
+    ),
+
+    Font(
+        resId = R.font.avenir_lt_medium,
+        weight = FontWeight.Medium
+    ),
+
+    Font(
+        resId = R.font.avenir_lt_black,
+        weight = FontWeight.Black
+    )
+)
+
+val CoreSans = FontFamily(
+    Font(
+        resId = R.font.core_sans_regular,
+        weight = FontWeight.Normal
+    ),
+
+    Font(
+        resId = R.font.core_sans_bold,
+        weight = FontWeight.Bold
+    )
+)
+
 val Typography = Typography(
+    titleMedium = TextStyle(
+        fontFamily = CoreSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = CoreSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = AvenirLT,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = CoreSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = CoreSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
