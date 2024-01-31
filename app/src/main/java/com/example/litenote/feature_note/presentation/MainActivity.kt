e9b7fe0076fe5c3ac6f8d8b12a79652d3464f2cb
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-01-31 23:37:17 +0530

    feat: add splash screen

--- app/src/main/java/com/example/litenote/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/litenote/feature_note/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.litenote.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.litenote.core.navigation.Navigation
 import com.example.litenote.ui.theme.LiteNoteTheme
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             LiteNoteTheme {
